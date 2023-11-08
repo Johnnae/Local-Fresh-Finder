1c1687c535989526f119518b104a5da3cb0f7987
JaeBEE <johnnae908@yahoo.com>
2023-11-07 19:55:14 -0800

    add vegs to farmers

--- server/models/farmer.js
+++ server/models/farmer.js
@@ -2,7 +2,7 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `saveds` array in User.js
 const farmerSchema = new Schema({
-  authors: [
+  veg: [
     {
       type: String,
     },
