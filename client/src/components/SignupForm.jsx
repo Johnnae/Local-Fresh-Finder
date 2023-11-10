f93ff1d772d324b108dfa80ed0810403eef8b19d
sthogue <sthogue16@gmail.com>
2023-11-09 19:09:29 -0600

    updated with ant design css package

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert } from 'antd';
 
 //import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
